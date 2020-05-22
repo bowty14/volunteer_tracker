30cfee934d36c19a32d4a4b2891a750b79583d41
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-22 11:10:25 -0700

    test for .find method. :pass.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -28,4 +28,16 @@ class Volunteer
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}' , #{@project_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    if volunteer
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id")
+      project_id = volunteer.fetch("project_id")
+      Volunteer.new({:name => name, :id => id, :project_id => project_id})
+    else
+      nil
+    end
+  end
 end
