d6078a2c415e40be99a0a0ca9d6dfbef8e9ec106
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-22 09:08:47 -0700

    test for .all :fail

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,30 +2,37 @@ require ('spec_helper')
 
 describe Project do
   describe '#title' do
-    it 'returns the project title' do
+    it('returns the project title') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       expect(project.title).to(eq('Teaching Kids to Code'))
     end
   end
 
-  context '#id' do
-    it 'returns the id of the project before saving project' do
+  context('#id') do
+    it('returns the id of the project before saving project') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       expect(project.id).to(eq(nil))
       end
     end
 
-    it 'returns the id of the project after saving project' do
+  context('#id') do
+    it('returns the id of the project after saving project') do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
       project.save
       expect(project.id).to(be_an_instance_of(Integer))
     end
-    
-    describe '#==' do
-      it 'is the same project if two projects have the same title' do
-        project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-        project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-        expect(project1 == project2).to(eq(true))
+  end
+
+  describe('#==') do
+    it('is the same project if two projects have the same title') do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project1 == project2).to(eq(true))
+    end
+  end
+    context('.all') do
+      it('is empty to start') do
+        expect(Project.all).to(eq([]))
       end
     end
 end
@@ -33,10 +40,6 @@ end
 
 
 
-  # context '.all' do
-  #   it 'is empty to start' do
-  #     expect(Project.all).to eq []
-  #   end
 
   #   it 'returns all projects' do
   #     project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
