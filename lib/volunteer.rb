6a8f4e37b02a6f0bce079c10722defbdb21faaf5
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-22 10:20:07 -0700

    pass first spec test.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,3 +1,10 @@
 class Volunteer
-  
+  attr_reader :id, :project_id
+  attr_accessor :name
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
+  end
 end
