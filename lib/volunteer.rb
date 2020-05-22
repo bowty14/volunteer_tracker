1fc4741b14a6da407dfb96fe551d41fbb26c6e57
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-22 11:05:28 -0700

    test for .all :pass

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -13,18 +13,19 @@ class Volunteer
   end
 
   def self.all
-    returned_volunteers = DB.exec("SELECT * FROM volunteers")
+    returned_volunteers = DB.exec('SELECT * FROM volunteers')
     volunteers = []
     returned_volunteers.each do |volunteer|
       name = volunteer.fetch("name")
       id = volunteer.fetch("id")
-      volunteers.push(Volunteer.new({:name => name, :id => id}))
+      project_id = volunteer.fetch("project_id")
+      volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
     end
     volunteers
   end
 
-  # def save 
-  #   result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
-  #   @id = result.first().fetch("id").to_i
-  # end
+  def save 
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}' , #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -23,13 +23,13 @@ describe(Volunteer) do
     end
   end
 
-  # describe('#save') do
-  #   it('adds a volunteer to the database') do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     expect(Volunteer.all).to(eq([volunteer1]))
-  #   end
-  # end
+  describe('#save') do
+    it('adds a volunteer to the database') do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      expect(Volunteer.all).to(eq([volunteer1]))
+    end
+  end
 
   context '.all' do
     it 'is empty to start' do
