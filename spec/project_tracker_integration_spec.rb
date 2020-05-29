92cb891c0bdec595710cafd122991605baea9598
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-28 18:40:29 -0700

    pass first integration test.

--- spec/project_tracker_integration_spec.rb
+++ spec/project_tracker_integration_spec.rb
@@ -7,11 +7,11 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
-# describe 'the project creation path', {:type => :feature} do
-#   it 'takes the user to the homepage where they can create a project' do
-#     visit '/'
-#     fill_in('title', :with => 'Teaching Kids to Code')
-#     click_button('Create Project')
-#     expect(page).to have_content('Teaching Kids to Code')
-#   end
-# end
+describe 'the project creation path', {:type => :feature} do
+  it 'takes the user to the homepage where they can create a project' do
+    visit '/'
+    fill_in('project_name', :with => 'Teaching Kids to Code')
+    click_button('Create project')
+    expect(page).to have_content('Teaching Kids to Code')
+  end
+end
