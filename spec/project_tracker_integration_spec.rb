ad974e05fdbff7a528ba5e938af9e4be71599d3f
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-28 18:47:17 -0700

    add 3rd integration test. expect to fail.

--- spec/project_tracker_integration_spec.rb
+++ spec/project_tracker_integration_spec.rb
@@ -28,3 +28,15 @@ describe 'the project update path', {:type => :feature} do
     expect(page).to have_content('Teaching Ruby to Kids')
   end
 end
+
+describe 'the project delete path', {:type => :feature} do
+  it 'allows a user to delete a project' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    id = test_project.id
+    visit "/projects/#{id}/edit"
+    click_button('Delete Project')
+    visit '/'
+    expect(page).not_to have_content("Teaching Kids to Code")
+  end
+end
