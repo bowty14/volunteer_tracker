4cf01dcb5ab58335c59636b19d929c5f1c7fe49b
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-28 18:41:22 -0700

    add 2nd integration test. expect to fail.

--- spec/project_tracker_integration_spec.rb
+++ spec/project_tracker_integration_spec.rb
@@ -15,3 +15,16 @@ describe 'the project creation path', {:type => :feature} do
     expect(page).to have_content('Teaching Kids to Code')
   end
 end
+
+describe 'the project update path', {:type => :feature} do
+  it 'allows a user to change the name of the project' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    visit '/'
+    click_link('Teaching Kids to Code')
+    click_link('Edit Project')
+    fill_in('title', :with => 'Teaching Ruby to Kids')
+    click_button('Update Project')
+    expect(page).to have_content('Teaching Ruby to Kids')
+  end
+end
